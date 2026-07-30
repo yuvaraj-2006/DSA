52dd8cb6522a449ebc755d870822eb03933f7611
YUVARAJ M <yuvaraj789007@gmail.com>
2026-07-30 15:35:15 +0530

    Time: 1 ms (9.93%) | Memory: 48.2 MB (34.70%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double myPow(double x, int n) {
+      long N=n;
+      if(N < 0){
+        x=1/x;
+        N=-N;
+      }
+      double ans=1;
+      while( N > 0 ){
+        if((N&1)==1){
+            ans*=x;
+        }
+        x=x*x;
+        N >>= 1;
+      }
+      return ans;
+    }
+}
